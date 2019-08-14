118b626aafca7a0a016e43780480fe3949934f3a
Francesco Sforazzini <francesco.sforazzini@gmail.com>
2019-08-14 11:58:11 +0200

    midify setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,6 @@ def install_deps():
     """Reads requirements.txt and preprocess it
     to be feed into setuptools.
 
-    This is the only possible way (we found)
-    how requirements.txt can be reused in setup.py
-    using dependencies from private github repositories.
-
-    Links must be appendend by `-{StringWithAtLeastOneNumber}`
-    or something like that, so e.g. `-9231` works as well as
-    `1.1.0`. This is ignored by the setuptools, but has to be there.
-
-    Warnings:
-        to make pip respect the links, you have to use
-        `--process-dependency-links` switch. So e.g.:
-        `pip install --process-dependency-links {git-url}`
-
     Returns:
          list of packages and dependency links.
     """
